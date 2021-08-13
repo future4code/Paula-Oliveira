aecb840257c93a7bda3c03cf439f6aed4a933dd4
Paula Rabelo de Oliveira <rabelooliveira.ap@gmail.com>
2021-08-13 11:10:07 -0300

    Adiçao de Resposta

--- semana01/funcoes/template_javascript/index.js
+++ semana01/funcoes/template_javascript/index.js
@@ -57,18 +57,19 @@
 
 "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
 
-
-
 Troque o nome, idade, cidade e se é estudante ou não por informações sobre você. 
 Lembrando que a função não possui entradas, apenas imprime essa mensagem. */
 
- function minhasInformacoes(){
+    function minhasInformacoes(){
 
-    console.log("Eu sou Paula, tenho 31 anos, moro em Macapá e sou estudante")
- }
+    const infoDados = ("Eu sou Paula, tenho 31 anos, moro em Macapá e sou estudante")
 
-    console.log(minhasInformacoes)
+    return infoDados
+     }
+    
+     console.log(minhasInformacoes)
 
+//----------------------------------------------------------------------------------------
 /*
 b)  Agora escreva uma função que receba 4 parâmetros 
 que correspondem às informações de uma pessoa: o nome (`string`), a idade (`number`), a cidade (`string`)
@@ -83,15 +84,89 @@ Eu sou [NOME], tenho [IDADE] anos, moro em [ENDEREÇO] e sou [PROFISSÃO].*/
 
     const dados = `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}`
     
-
     return dados
  }
  
-    const nome = prompt("Qual o seu nome?")
-    const idade = prompt("Qual o sua idade?")
-    const cidade = prompt("Onde você mora?")
-    const profissao = prompt("Qual a sua profissão?")
+        const novoNome = prompt("Qual o seu nome?")
+        const novaIdade = Number(prompt("Qual o sua idade?"))
+        const novaCidade = prompt("Onde você mora?")
+        const novaProfissao = prompt("Qual a sua profissão?")
 
-    console.log(nome, idade, cidade, profissao)
+    //console.log(nome, idade, cidade, profissao)
 
-    const informacaoCapturada = informacaoDoUsuario(nome, idade, cidade, profissao)
+    const informacaoCapturada = informacaoDoUsuario(novoNome, novaIdade, novaCidade, novaProfissao)
+
+    console.log(informacaoCapturada)
+
+
+
+    /*a) Escreva uma função que receba 2 números como parâmetros, e, 
+    dentro da função,     faça a soma das duas entradas e retorne o 
+    resultado. Invoque a função e imprima no console o resultado. */
+
+    
+    function somar(n1, n2){
+        const fazSoma = (n1 + n2)
+
+        return fazSoma
+    }
+
+    const numeroUm = Number(prompt("Digite um número da sorte."))
+    const numeroDois = Number(prompt("Agora digite outro número. Só para aumentar a sorte!"))
+
+    const valorParaSomar = somar(numeroUm, numeroDois)
+
+    console.log(valorParaSomar)
+
+
+    /*b) Faça uma função que recebe 2 números e retorne um booleano que 
+    informa se o primeiro número é **maior ou igual** ao segundo. */
+
+    function numeroMaior(novoNumero, novoNumeroDois){
+        const compara = (novoNumero >= novoNumeroDois)
+
+        return compara
+    }
+    const numeroInserido = Number(prompt("Digite um número."))
+    const numeroInseridoDois = Number(prompt("Digite outro número."))
+
+    const numeroDeComparacao = numeroMaior(numeroInserido, numeroInseridoDois)
+
+    console.log("Seu primeiro número é maior ou igual ao segundo.", numeroDeComparacao)
+
+
+    /*c) Escreva uma função que receba um número e devolva um booleano 
+    indicando se ele é par ou não */
+
+    //function recebeUmNumero (numeroRecebido){
+        
+
+    //}
+
+
+    /*d) Faça uma função que recebe uma mensagem (`string`) como parâmetro 
+    e imprima o tamanho dessa mensagem, juntamente com uma versão dela em 
+    letras maiúsculas. */
+
+   /*  
+Faça um programa que receba o tamanho da base 
+e altura de um triangulo 
+em centímetros e retorne a área desse triangulo também em centímetros.
+
+Exemplo de entrada: calculaArea(10, 20)
+Exemplo de saída: A área tem 100 cm.
+*/
+
+
+function areaTriangulo (base, altura){
+
+    const somaDaAreaTriangulo = (base * altura) / 2 
+
+    return somaDaAreaTriangulo
+}
+
+const calculaArea = areaTriangulo(10,20)
+
+console.log(calculaArea)
+
+
