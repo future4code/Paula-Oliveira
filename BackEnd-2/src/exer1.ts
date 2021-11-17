cd302168a017fc696cb74b9e52ea750098b839ea
Paula Rabelo de Oliveira <rabelooliveira.ap@gmail.com>
2021-11-17 19:48:38 -0300

    Exercicio1

---
+++ BackEnd-2/src/exer1.ts
@@ -0,0 +1,16 @@
+console.log("Hello World")
+//a)
+const minhaString:string = "valor"
+//se atribuir valor numérico ele não vai aceitar e dará erro. 
+
+//b)
+const meuNumero:number | string = 34
+
+//c)
+
+type pessoa ={
+    nome:string,
+    idade:number,
+    corFavorita:string
+}
+
