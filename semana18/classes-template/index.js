1df35514207265f65ed85b78b3b3630dd5a6e9a1
Paula Rabelo de Oliveira <rabelooliveira.ap@gmail.com>
2021-12-06 22:56:36 -0300

    aula52

---
+++ semana18/classes-template/index.js
@@ -0,0 +1,11 @@
+"use strict";
+class UserAccount {
+    constructor(cpf, name, age) {
+        this.balance = 0;
+        console.log("Chamando o construtor da classe UserAccount");
+        this.cpf = cpf;
+        this.name = name;
+        this.age = age;
+    }
+}
+//# sourceMappingURL=index.js.map
---
+++ semana18/classes-template/index.ts
@@ -0,0 +1,26 @@
+// a) Para que serve o construtor dentro de uma classe e como fazemos para chamá-lo?
+// Para ter ações executadas ao criar uma instância da classe.
+
+//b*) Copie o código abaixo para o seu exercício de hoje; crie uma instância dessa classe 
+//(dê o nome, cpf e idade que você quiser). Quantas vezes a mensagem `"Chamando o construtor
+// da classe User"` foi impressa no terminal?*
+
+class UserAccount {
+    private cpf: string;
+    private name: string;
+    private age: number;
+    private balance: number = 0;
+    // private transactions: Transaction[] = [];
+  
+    constructor(
+       cpf: string,
+       name: string,
+       age: number,
+    ) {
+       console.log("Chamando o construtor da classe UserAccount")
+       this.cpf = cpf;
+       this.name = name;
+       this.age = age;
+    }
+  
+  }
