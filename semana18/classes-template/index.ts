7503efcda24aa2184d28fdd781367d4a7741c508
Paula Rabelo de Oliveira <rabelooliveira.ap@gmail.com>
2021-12-07 21:51:19 -0300

    modificacao

--- semana18/classes-template/index.ts
+++ semana18/classes-template/index.ts
@@ -5,22 +5,26 @@
 //(dê o nome, cpf e idade que você quiser). Quantas vezes a mensagem `"Chamando o construtor
 // da classe User"` foi impressa no terminal?*
 
+
+
 class UserAccount {
     private cpf: string;
     private name: string;
     private age: number;
     private balance: number = 0;
-    // private transactions: Transaction[] = [];
+    private transactions: Transaction[] = [];
   
     constructor(
-       cpf: string,
-       name: string,
-       age: number,
-    ) {
-       console.log("Chamando o construtor da classe UserAccount")
-       this.cpf = cpf;
-       this.name = name;
-       this.age = age;
-    }
+      cpf: string,
+      name: string,
+      age: number,
+   ) {
+      console.log("Chamando o construtor da classe UserAccount")
+      this.cpf = cpf;
+      this.name = name;
+      this.age = age;
+      
+   }
+    
   
   }
